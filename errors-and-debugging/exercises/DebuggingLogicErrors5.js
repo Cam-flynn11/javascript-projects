059ceca0366d36ab4690e325d6e7d0bf7aafdf1c
Cam-flynn11 <caitlin.flynn4311@gmail.com>
2024-05-01 22:10:24 -0500

    <Modified second if/else block to chane variable and added last if/else block.

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -3,6 +3,7 @@
 // Refactor the code to do this. Verify that your change works by updating the console.log statements.
 
 let launchReady = false;
+let crewReady = false;
 let fuelLevel = 17000;
 let crewStatus = true;
 let computerStatus = 'green';
@@ -15,14 +16,20 @@ if (fuelLevel >= 20000) {
    launchReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("launchready = ", launchReady);
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
-   launchReady = true;
+   crewReady = true;
 } else {
    console.log('WARNING: Crew or computer not ready!');
-   launchReady = false;
+   crewReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("crewready = ", crewReady);
+
+if (launchReady && crewReady === "true") {
+   console.log('10...9...8...7...6...5...4...3...2...1... Liftoff!')
+} else {
+   console.log('Launch Scrubbed')
+}
