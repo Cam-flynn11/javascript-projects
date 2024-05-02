a33558347a5f78c420ed6c576cafbaccb6f45f1f
Cam-flynn11 <caitlin.flynn4311@gmail.com>
2024-05-01 22:15:15 -0500

    <the 'L' was not capital in Level everywhere it was used, changed that.>

--- errors-and-debugging/exercises/DebuggingRuntimeErrors1.js
+++ errors-and-debugging/exercises/DebuggingRuntimeErrors1.js
@@ -1,10 +1,10 @@
 //Run this code first and examine the error message.
 //Pay close attention to any line numbers mentioned in the message - these will help locate and repair the mistake in the code.
-
+   // the 'L' in Level was not capital in all the right places - changed that
 let launchReady = false;
 let fuelLevel = 17000;
 
-if (fuellevel >= 20000) {
+if (fuelLevel >= 20000) {
    console.log('Fuel level cleared.');
    launchReady = true;
 } else {
