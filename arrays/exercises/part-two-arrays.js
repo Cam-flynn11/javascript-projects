af5daa4010d93675938d986f8bbe8df196d93b08
Cam-flynn11 <caitlin.flynn4311@gmail.com>
2024-05-07 23:05:24 -0500

    <updated completed arrays>

--- arrays/exercises/part-two-arrays.js
+++ arrays/exercises/part-two-arrays.js
@@ -1,7 +1,7 @@
 let cargoHold = ['oxygen tanks', 'space suits', 'parrot', 'instruction manual', 'meal packs', 'slinky', 'security blanket'];
 
 //1) Use bracket notation to replace ‘slinky’ with ‘space tether’. Print the array to confirm the change.
-cargoHold.splice(5, 1, 'space tether');
+cargoHold[5] = 'space tether';
 console.log(cargoHold);
 //2) Remove the last item from the array with pop. Print the element removed and the updated array.
 console.log(cargoHold.pop());
