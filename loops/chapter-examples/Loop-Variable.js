2c878ed7f5b26eccb4cb6d4d8627f1165b787f03
Cam-flynn11 <caitlin.flynn4311@gmail.com>
2024-05-13 22:08:44 -0500

    practice success

--- loops/chapter-examples/Loop-Variable.js
+++ loops/chapter-examples/Loop-Variable.js
@@ -1,5 +1,17 @@
 // Experiment with this loop by modifying each of the following: the variable initialization, the boolean condition, and the update expression.
 
-for (let i = 0; i < 51; i++) {
-   console.log(i);
- }
+// for (let i = 0; i < 51; i++) {
+//    console.log(i);
+//  }
+
+//  for(let i = 3; i < 51; i++) {
+//   console.log(i);
+//  }
+
+// for(let i = 0; i < 32; i++) {
+//   console.log(i);
+// }
+
+for(let i = 0; i < 51; i = i + 5) {
+  console.log(i);
+}
