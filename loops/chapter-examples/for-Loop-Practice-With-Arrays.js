41af8fe18b7cc9f1822b06f58f96d5a36967c444
Cam-flynn11 <caitlin.flynn4311@gmail.com>
2024-05-13 21:57:48 -0500

    practice success

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,6 @@
 // create an array variable containing the names
-
+let famNames = ['Cam', 'Chloe', 'Star', 'Moon', 'Augie', 'Adam', 'Laura'];
 // write a for loop that prints each name on a different line
+for(let i = 0; i < famNames.length; i++){
+    console.log(famNames[i]);
+}
