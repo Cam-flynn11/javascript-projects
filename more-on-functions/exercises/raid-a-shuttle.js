90f36d3a7cee4ae98d81c68bebb5814dec0cb67c
Cam-flynn11 <caitlin.flynn4311@gmail.com>
2024-06-05 00:36:56 -0500

    i cant figure out why it is still saying 'and undefined' in my output

--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -21,12 +21,37 @@ function holdStatus(arr){
 let fuelLevel = 200000;
 let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold', 'water', 'AE-35 unit'];
 
-console.log("Fuel level: " + checkFuel(fuelLevel));
-console.log("Hold status: " + holdStatus(cargoHold));
+let fuelStorage = function(fuelLevel) {
+  if(checkFuel(fuelLevel) === 'green') {
+    return fuelLevel - 100001
+  } else if (checkFuel(fuelLevel) === 'yellow') {
+    return fuelLevel - 50001
+  } else {
+    return fuelLevel;
+  }
+}
 
-/* Steal some fuel from the shuttle:
- * /
- 
+let cleanUp = [];
+
+let extraCargo = function(cargoHold) {
+  cargoHold = cargoHold.splice(3, 2, 'tv dish', 'rocks');
+  cleanUp.push(cargoHold)
+
+  return cleanUp;
+} 
+
+let irs = function (fuelLevel, cargoHold) {
+  fuelStorage(fuelLevel);
+  extraCargo(cargoHold);
+}
+
+console.log(extraCargo(cargoHold));
+console.log("Fuel level: " + checkFuel(fuelLevel));
+console.log("Fuel Storage: " + fuelStorage(fuelLevel));
+console.log("Hold status: " + holdStatus(cargoHold));
+console.log(`Raided ${fuelStorage(fuelLevel)} kg of fuel from the tanks, and stole ${cleanUp[0]} and ${cleanUp[1]} from the cargo hold.`)
+
+// /* Steal some fuel from the shuttle:*/
 //a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
 
 //b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
@@ -35,8 +60,7 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 //d). Decide where to best place your function call to gather our new fuel.
 
-/* Next, liberate some of that glorious cargo.
- * /
+/* Next, liberate some of that glorious cargo.*/
 
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
 
@@ -46,12 +70,10 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 //d). Don’t get hasty, matey! Remember to test your function.
 
-/* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
- * /
+/* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.*/
  
 //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
 	
 //b). Call your anonymous fuel and cargo functions from within irs.
 
-//c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."
-
+//c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."*/
