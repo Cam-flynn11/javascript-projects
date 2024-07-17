3095cd9105368f7b3fa2f1f4ba712916874f6237
Cam-flynn11 <caitlin.flynn4311@gmail.com>
2024-07-17 11:11:52 -0500

    attempted

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -4,6 +4,11 @@ function gradeLabs(labs) {
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
+  try {
+    console.log('lab.runLab is not a function')
+  } catch(err) {
+    console.log('Error Thrown');
+  }
 }
 
 let studentLabs = [
@@ -21,4 +26,27 @@ let studentLabs = [
   }
 ];
 
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
 gradeLabs(studentLabs);
+gradeLabs(studentLabs2);
+
