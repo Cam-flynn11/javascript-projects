7ebea18b9251c931ce13bcb3ff84998647dac119
Cam-flynn11 <caitlin.flynn4311@gmail.com>
2024-08-07 10:15:56 -0500

    completed

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -2,7 +2,25 @@ function init () {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
+    let backgroundColor = "";
+button.addEventListener('click', event => {
+    paragraph.innerHTML = 'Houston! We have liftoff!';
+});
+missionAbort.addEventListener('mousein', event => {
+    backgroundColor.color = "red";
+});
+missionAbort.addEventListener('mouseout', (event) => {
+    event.target.style.backgroundColor = "";
+});
+missionAbort.addEventListener('click', (event) => {
+   let confirmation = window.confirm("Are you sre you want to abort the mission");
+  
+   if (confirmation === true) {
+   let newStatus = document.getElementById("statusReport");
 
+   newStatus.innerHTML = "Mission Aborted! Space shuttle returning home"
+  }  
+})
     // Put your code for the exercises here.
     
 }
